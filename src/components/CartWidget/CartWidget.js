531af3252967732dfb8096c1fe60a224d6fb3114
Lucas Monti <lucassmonti@gmail.com>
2024-09-21 13:03:54 -0300

    preentrega1

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import cart from "./assets/carritocompras.jpg"
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="carritocompras" />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
